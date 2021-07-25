085d387299011ea70cd969d7504ee3a5c4650b31
Tavo Tinajero <tavo.tinajero@gmail.com>
2021-07-25 07:07:54 -0500

    cambios
    pequeños cambios

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,25 +2,38 @@ import React from 'react'
 import { Card } from 'antd'
 import { Row, Col } from 'antd';
 import { FaKeyboard,FaLaptop, FaCode } from 'react-icons/fa';
+import LoadingBlock from './LoadingBlock';
 
-function Summary() {
-    return (
-        <Row>
-            <Col span={24} >
-                <div style={{textAlign: 'justify', padding: '30px 30px 0px 30px'}}>
-                    <Card title="Summary" style={{ fontWeight: 'normal', fontSize:17 }}>
-                        Full Stack Developer with more than 15 years of experience in application and web development (Frontend and Backend), implementing CI / CD, using 
-                        methodologies (SCRUM, Kanban) and infrastructure management with the services of AWS Web Services.Highly efficient, results-oriented with a proven 
-                        ability to effectively develop, manage and control the requirements that the company needs. I like challenging projects and being able to work under pressure.
-                        <br/><br/>
-                        <div style={{textAlign: 'center'}}>
-                            <FaKeyboard size={25} style={{ fill: '#bcdae9'}}/>  <FaLaptop size={25} style={{ fill: '#bcdae9'}}/>  <FaCode size={25} style={{ fill: '#bcdae9'}}/>
+const Summary = (estado) => {
+    
+    if(estado) {
+        return (
+            <>
+                <Row>
+                    <Col span={24} >
+                        <div style={{textAlign: 'justify', padding: '30px 30px 0px 30px'}}>
+                            <Card title="Summary" style={{ fontWeight: 'normal', fontSize:17 }}>
+                                Full Stack Developer with more than 15 years of experience in application and web development (Frontend and Backend), implementing CI / CD, using 
+                                methodologies (SCRUM, Kanban) and infrastructure management with the services of AWS Web Services.Highly efficient, results-oriented with a proven 
+                                ability to effectively develop, manage and control the requirements that the company needs. I like challenging projects and being able to work under pressure.
+                                <br/><br/>
+                                <div style={{textAlign: 'center'}}>
+                                    <FaKeyboard size={25} style={{ fill: '#bcdae9'}}/>  <FaLaptop size={25} style={{ fill: '#bcdae9'}}/>  <FaCode size={25} style={{ fill: '#bcdae9'}}/>
+                                </div>
+                            </Card>
                         </div>
-                    </Card>
-                </div>
-            </Col>
-        </Row>
-    )
+                    </Col>
+            </Row>
+            </>
+        )
+    } else {
+        return (
+            <LoadingBlock />
+        )
+    }
+    
+    
 }
 
 export default Summary
+
--- src/pages/Cv.jsx
+++ src/pages/Cv.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import DynamicContainer from '../components/DynamicContainer';
 import LoadingText from '../components/LoadingText';
+import Summary from '../components/Summary';
 import Title from '../components/Title';
 
 
@@ -8,12 +9,11 @@ const Cv = () => {
 
     const [estatus, SetCount] = useState(0)
     
-
     return (
         <>
         <Title />
         <LoadingText />
-        <DynamicContainer estado="1" nombre="Summary" />
+        <Summary estado={estatus} />
         <DynamicContainer estado="1" nombre="Competencies" />
         <DynamicContainer estado="0" nombre="Experience" />
         <DynamicContainer estado="0" nombre="Education" />
