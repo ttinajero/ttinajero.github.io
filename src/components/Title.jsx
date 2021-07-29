68deedf571b1ce2c9e355301da7fa7f7e5009c39
Tavo Tinajero <tavo.tinajero@gmail.com>
2021-07-29 16:15:14 -0500

    se agrego la imagen de logo

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Row, Col, Typography } from 'antd';
 import { FaGithub } from 'react-icons/fa';
 
+import logo from '../icon_otg.png';
 
 const { Link } = Typography;
 
@@ -9,8 +10,11 @@ const Title = () => {
     return (
         <Row>
             <Col span={24} >
-                <div style={{textAlign: 'left', padding: '30px 0px 0px 30px', fontSize:60, fontFamily: 'Ubuntu'}}>
-                    Octavio Tinajero / <Link href="https://github.com/ttinajero/ttinajero.github.io" target="_blank"><FaGithub size={38} style={{ fill: '#800180'}} /></Link>
+                <div>
+                <img src={logo} />
+                </div>
+                <div style={{textAlign: 'center', padding: '30px 0px 0px 30px', fontSize:60, fontFamily: 'Ubuntu'}}>
+                   Octavio Tinajero / <Link href="https://github.com/ttinajero/ttinajero.github.io" target="_blank"><FaGithub size={38} style={{ fill: '#800180'}} /></Link>
                  </div>
             </Col>
         </Row>
