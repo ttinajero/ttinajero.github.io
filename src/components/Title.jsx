a5f31e99f0e680c493096b9d4a58622934de9335
Tavo Tinajero <tavo.tinajero@gmail.com>
2021-08-01 16:45:14 -0500

    Se cambio la imagen

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,6 +3,7 @@ import { Row, Col, Typography } from 'antd';
 import { FaGithub } from 'react-icons/fa';
 
 import logo from '../icon_otg.png';
+import logo2 from '../cv_pic_trans.png';
 
 const { Link } = Typography;
 
@@ -11,7 +12,7 @@ const Title = () => {
         <Row>
             <Col span={24} >
                 <div>
-                <img src={logo} alt="Imagen de Logo"/>
+                <img src={logo2} alt="Imagen de Logo" width="500px"/>
                 </div>
                 <div style={{textAlign: 'center', padding: '30px 0px 0px 30px', fontSize:60, fontFamily: 'Ubuntu'}}>
                    Octavio Tinajero / <Link href="https://github.com/ttinajero/ttinajero.github.io" target="_blank"><FaGithub size={38} style={{ fill: '#800180'}} /></Link>
